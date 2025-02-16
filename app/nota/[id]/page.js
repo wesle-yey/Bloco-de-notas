e1711af05a65f10c3d4293ee7db365ae775f5d2d
Wesley <clashcomifeeh@gmail.com>
2025-02-16 09:57:15 -0300

    Atualizações na rota nota/id

--- app/nota/[id]/page.js
+++ app/nota/[id]/page.js
@@ -1,35 +1,10 @@
 'use client';
-
-import axios from "axios";
-import { useParams } from "next/navigation";
-import { useState, useEffect } from "react";
+import Caderneta from "../../components/Caderneta"
 
 export default function NotasPage() {
-    const { id } = useParams();
-    const [nota, setNota] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        if (!id) return;
-
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(response => {
-                setNota(response.data);
-                setLoading(false);
-            })
-            .catch(error => {
-                console.error(error);
-                setLoading(false);
-            });
-    }, [id]);
-
-    if (loading) return <p>Carregando...</p>;
-    if (!nota) return <p>Nota não encontrada.</p>;
-
     return (
         <div>
-            <h3>Title: {nota.title}</h3>
-            <p>Content: {nota.body}</p>
+            <Caderneta/>
         </div>
     );
 }
