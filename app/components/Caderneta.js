91d58d1f43cf4e0c4f04d18b057e5e870546f79a
wesle-yey <clashcomifeeh@gmail.com>
2025-03-03 11:21:56 -0300

    Método PUT

--- app/components/Caderneta.js
+++ app/components/Caderneta.js
@@ -3,19 +3,30 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../App.css';
 import axios from "axios";
-import { useParams } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
 
 function Caderneta() {
     const { id } = useParams();
     const [nota, setNota] = useState("");
     const [loading, setLoading] = useState(true);
-    const cadernoRef = useRef(null); 
+    const cadernoRef = useRef(null);
+    const titleRef= useRef(null);
+
+    function putNote() {
+        axios.put(`https://jsonplaceholder.typicode.com/posts/${id}`,
+            {
+                title:nota.title,
+                body:nota.body
+            }).then(
+                redirect('/')
+            )
+    }
 
     useEffect(() => {
         if (!id) return;
 
-        //axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        axios.get(`http://localhost:8080/documentos/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+            //axios.get(`http://localhost:8080/documentos/${id}`)
             .then(response => {
                 setNota(response.data || "Escreva aqui...");
                 setLoading(false);
@@ -28,7 +39,13 @@ function Caderneta() {
 
     useEffect(() => {
         if (cadernoRef.current) {
-            cadernoRef.current.innerHTML = nota.conteudo; // Atualiza o conteúdo sem conflitos com React
+            cadernoRef.current.innerHTML = nota.body; // Atualiza o conteúdo sem conflitos com React
+        }
+    }, [nota]);
+
+    useEffect(() => {
+        if (titleRef.current) {
+            titleRef.current.innerHTML = nota.title; // Atualiza o conteúdo sem conflitos com React
         }
     }, [nota]);
 
@@ -36,9 +53,12 @@ function Caderneta() {
 
     return (
         <div>
-            <h1>{nota.titulo}</h1>
-        <div ref={cadernoRef} className="caderno" contentEditable="true"
-        />
+            <h1 ref={titleRef} contentEditable="true">{nota.title}</h1>
+            <div ref={cadernoRef} className="caderno" contentEditable="true"
+            />
+            <div>
+                <button onClick={() => putNote()}>Salvar</button>
+            </div>
         </div>
     );
 }
